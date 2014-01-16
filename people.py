86702bc8286482f0bee185f08c05b85bc177c996
eduardo <piscosour@gmail.com>
2014-01-16 15:07:28 -0500

    adding characters to agnes

--- people.py
+++ people.py
@@ -9,21 +9,21 @@ class Person:
 people = [
 
 	Person(name='sam',
-	       description=''),
+	       description='Mission commander.'),
 
 	Person(name='alex',
-		   description=''),
+		   description='Lead pilot'),
 
 	Person(name='gordon',
 		   description='Auxiliary pilot and Agnes on board maintenance engineer. Previously research scientist at Black Mesa Lambda compound, involved in the incident. Selected for this mission because of his acquaintance with extraterrestrial life forms.'),
 
 	Person(name='gladys',
-		   description=''),
+		   description='Operations officer. Previously support officer in the Aperture Science main research and testing facility, in charge of human subject testing and facilities management.'),
 
 	Person(name='dave',
-		   description=''),
+		   description='Lead on board research scientist. Machine hater. Future star child.'),
 
-	Person(name='jack',
+	Person(name='jim',
 		   description='Lead designer and product manager in the Dunder Mifflin Paper and Aerospace Company, assigned to project Agnes. Previously worked at the Scranton research and development laboratory. Responsible for the design, development and implementation of Agnes prior to mission launch.')
 
 ]
